180db9e605e5ce71822dcb4f66fec427a03eb0a5
Eduardo Mendonça <57375165+DuMendonca@users.noreply.github.com>
2026-02-01 00:38:27 +0000

    feat: add methods for instructions

--- analyzer-domain.py
+++ analyzer-domain.py
@@ -1,38 +1,41 @@
-# Ler o arquivo de definição da linguagem
-# Ler linha a linha e interpretar os comandos
-# Alguns comandos tem valores associados (ex: N 10, S 5, E 3, W 2)
-# O foco dos comandos é montar um desenho baseado nas instruções dadas
-
 class AnalyzerDomain:
     
-    _commands = {
-        'P': 'select_pen',
-        'D': 'pen_down',
-        'U': 'pen_up',
-        'N': 'move_north',
-        'S': 'move_south',
-        'E': 'move_east',
-        'W': 'move_west',
-        'PA': 'print_area'
-    }
-
     def __init__(self):
         self.instructions = []
+        self._commands = {
+            'P': ('ARG', self.doSelectPen),
+            'D': ('NO_ARG', self.doPenDown),
+            'U': ('NO_ARG', self.doPenUp),
+            'N': ('ARG', self.doMoveNorth),
+            'S': ('ARG', self.doMoveSouth),
+            'E': ('ARG', self.doMoveEast),
+            'W': ('ARG', self.doMoveWest),
+            'PA': ('ARG', self.doPrintArea)
+        }
 
     def interpret_line(self, line):
         parts = line.split()
         command = parts[0]
 
-        if (len(parts) > 1 and parts[1].isdigit()):
-            value = int(parts[1]) 
-        else: 
-            value = None
+        if (len(parts) > 1 and parts[1].isdigit()): value = int(parts[1]) 
+        else: value = None
 
         if command in self._commands:
-            self.instructions.append((self._commands[command], value))
+            args, method = self._commands[command]
+            self.instructions.append((args, method.__name__, value))
         else:
             raise ValueError(f"Comando desconhecido: {command}")
     
+    def execute_instruction(self, instructions):
+        for instruction in instructions:
+            args, method_name, value = instruction
+            method = getattr(self, method_name)
+
+            if args == 'ARG': method(value)
+            elif args == 'NO_ARG': method()
+            else: raise ValueError(f"Tipo de argumento desconhecido: {args}")
+                
+
     def interpret(self, lines):
         for line in lines:
             self.interpret_line(line.strip())
@@ -47,7 +50,39 @@ class AnalyzerDomain:
     def get_instructions(self):
         print(self.instructions)
 
+    def doSelectPen(self, arg):
+        print(f"Selecionou a caneta {arg}")
+        return
+    
+    def doPenDown(self):
+        print("Caneta para baixo")
+        return
+    
+    def doPenUp(self):
+        print("Caneta para cima")
+        return
+    
+    def doMoveNorth(self, arg):
+        print(f"Moveu para norte {arg} unidades")                       
+        return
+    
+    def doMoveSouth(self, arg):
+        print(f"Moveu para sul {arg} unidades")
+        return
+
+    def doMoveEast(self, arg):
+        print(f"Moveu para leste {arg} unidades")
+        return
+    
+    def doMoveWest(self, arg):
+        print(f"Moveu para oeste {arg} unidades")
+        return
+
+    def doPrintArea(self, arg):
+        print(f"Imprimindo área {arg}")
+        return
+
 if __name__ == "__main__":
     analyzer = AnalysisLanguage() 
     analyzer.load_from_file('language.txt')
-    analyzer.get_instructions()
+    analyzer.execute_instruction(analyzer.instructions)
